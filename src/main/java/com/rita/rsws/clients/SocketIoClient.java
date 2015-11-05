8c95c9ed1f6f5cedf690af36a5cc7e94c65b389a
shaunyip <shaunyip@outlook.com>
2015-11-05 19:35:43 +0800

    arguments

--- src/main/java/com/rita/rsws/clients/SocketIoClient.java
+++ src/main/java/com/rita/rsws/clients/SocketIoClient.java
@@ -1,8 +1,11 @@
 package com.rita.rsws.clients;
 
 import io.socket.client.IO;
+import io.socket.client.Manager;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
+import io.socket.emitter.Emitter.Listener;
+import io.socket.engineio.client.Transport;
 
 import java.net.URISyntaxException;
 import java.text.MessageFormat;
@@ -21,17 +24,18 @@ import com.rita.rsws.RokuServer;
  */
 public class SocketIoClient {
 
-	public static final String EVENT_STATUS = "status";
+	public static final String STATUS = "status";
 	public static final String ADD_DEVICE = "addDevcie";
- 
+
+	public static final String DEVICE_CONNECTED = "deviceConnected";
+	public static final String TV = "tv";
+	public static final String CATPTURE = "capture";
 
 	private RokuServer rokuServer;
 
 	private Socket socket;
 
 	private String sioServerUrl;
-	
-	
 
 	private static Logger verboseLogger = LoggerFactory
 			.getLogger("verboseLogger");
@@ -39,14 +43,13 @@ public class SocketIoClient {
 	private static Logger logger = LoggerFactory
 			.getLogger(SocketIoClient.class);
 
- 
- 
 	public SocketIoClient(String sioServerUrl) {
 		this.sioServerUrl = sioServerUrl;
 	}
 
 	public void start() {
 		socket = connectSocketIoServer();
+
 		socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
 
 			@Override
@@ -64,10 +67,59 @@ public class SocketIoClient {
 								+ sioServerUrl);
 			}
 
-		}).on(EVENT_STATUS, new Emitter.Listener() {
+		}).on(STATUS, new Emitter.Listener() {
 			@Override
 			public void call(Object... args) {
-				JSONObject jsonObj = getJsonDataFromEventAndLog(EVENT_STATUS, args);
+				JSONObject jsonObj = getJsonDataFromEventAndLog(STATUS, args);
+				String deviceId = getDeviceIdAndLog(jsonObj);
+				if (deviceId == null) {
+					return;
+				}
+				try {
+					rokuServer.sendMsgToRoku(deviceId, jsonObj);
+				} catch (Exception e) {
+					logSendToRokuException(jsonObj, e);
+
+				}
+			}
+
+		}).on(DEVICE_CONNECTED, new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject jsonObj = getJsonDataFromEventAndLog(
+						DEVICE_CONNECTED, args);
+				String deviceId = getDeviceIdAndLog(jsonObj);
+				if (deviceId == null) {
+					return;
+				}
+				try {
+					rokuServer.sendMsgToRoku(deviceId, jsonObj);
+				} catch (Exception e) {
+					logSendToRokuException(jsonObj, e);
+
+				}
+			}
+
+		}).on(CATPTURE, new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject jsonObj = getJsonDataFromEventAndLog(CATPTURE, args);
+				String deviceId = getDeviceIdAndLog(jsonObj);
+				if (deviceId == null) {
+					return;
+				}
+				try {
+					rokuServer.sendMsgToRoku(deviceId, jsonObj);
+				} catch (Exception e) {
+					logSendToRokuException(jsonObj, e);
+
+				}
+			}
+
+		}).on(TV, new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject jsonObj = getJsonDataFromEventAndLog(TV, args);
 				String deviceId = getDeviceIdAndLog(jsonObj);
 				if (deviceId == null) {
 					return;
@@ -81,6 +133,15 @@ public class SocketIoClient {
 			}
 
 		});
+
+		socket.io().on(Manager.EVENT_TRANSPORT, new Listener() {
+			@Override
+			public void call(Object... args) {
+				//Transport transport = (Transport) args[0];
+				verboseLogger.info("socket.io client has got something from the server side!");
+			}
+		});
+
 		socket.connect();
 	}
 
@@ -125,13 +186,11 @@ public class SocketIoClient {
 		}
 	}
 
- 
-
 	public void sendMsgToSio(String event, JSONObject jsonObj) {
 		socket.emit(event, jsonObj);
-		
+
 	}
-	
+
 	public void setRokuServer(RokuServer rokuServer) {
 		this.rokuServer = rokuServer;
 	}
